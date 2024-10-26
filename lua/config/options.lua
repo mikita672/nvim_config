f65573812ffdff7f44bccb8e98f8664db62f8ee7
Mikita Dzeviatau <81618202+mikita672@users.noreply.github.com>
2024-10-26 19:53:56 +0200

    Blocked arrows keys

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,20 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.clipboard = unnamedplus
+
+-- Disable arrow keys in normal, insert, and visual modes
+vim.api.nvim_set_keymap("n", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Right>", "<Nop>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("i", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<Right>", "<Nop>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("v", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<Right>", "<Nop>", { noremap = true, silent = true })
